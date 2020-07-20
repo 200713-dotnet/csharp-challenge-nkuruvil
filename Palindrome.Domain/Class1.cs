f510c34874ab6bec7e2b66741ae379e82b2cb5cc
nkuruvil <noahk0597@gmail.com>
2020-07-20 15:57:32 -0500

    Palindrome Code

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 
+
 namespace Palindrome.Domain
 {
     public class Class1
     {
+
+        static void Main()
+        {
+           Console.WriteLine("Welcome to Palindrome Checker. Enter word or phrase.");
+            var phrase = System.Console.ReadLine();
+            int phraseLength = phrase.Length;
+            Boolean isPalindrome = true;
+
+            for(int i=0; i<phraseLength/2; i++){
+                if(phrase[i] != phrase[phraseLength-i-1]){
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if(isPalindrome) {
+                Console.WriteLine($"{phrase} is palindrome");
+            } else {
+                Console.WriteLine($"{phrase} is not Palindrome");
+            }
+        }
+
+
+
     }
 }
